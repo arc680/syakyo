6abf9bef28b52080746be41f80d64b2b6a430a00
arc680 <arc680@live.com>
2017-02-03 23:40:52 +0900

    P.74

---
+++ javascript/chap2/logical2.js
@@ -0,0 +1,7 @@
+var x = 1;
+var y = 2;
+
+// console.log(x === 1 && y === 1);
+// console.log(x === 1 || y === 1);
+if (x === 1) console.log('hello');
+x === 1 && console.log('hello');
---
+++ javascript/chap2/shortcut.js
@@ -0,0 +1,3 @@
+var msg = '';
+msg = msg || 'hello world';
+console.log(msg);
