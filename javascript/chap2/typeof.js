921d36223759016c8101bc8900d3ae8dd0f9c9ad
arc680 <arc680@live.com>
2017-02-03 23:49:04 +0900

    P.77

---
+++ javascript/chap2/typeof.js
@@ -0,0 +1,14 @@
+var num = 1;
+console.log(typeof num);
+
+var str = 'aaaaaa';
+console.log(typeof str);
+
+var flag = true;
+console.log(typeof flag);
+
+var ary = ['A', 'B', 'C'];
+console.log(typeof ary);
+
+var obj = {x:1, y:2};
+console.log(typeof obj);
